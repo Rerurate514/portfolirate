72cdf3024879f1a410e8312fc97649d6d989d51b
Rerurate514 <rerurateyuto@gmail.com>
2024-06-26 22:02:20 +0900

    sectionコンポーネントの作成

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -2,32 +2,36 @@ import "../css/section.css"
 
 export const Section = () => {
     return (
-        <div className="futura-font-light margin-32">
+        <div className="futura-font-light margin-32 width-side">
             <div className="flex padding-16 justify-bw underLine font-64">
                 <div>
                     <strong className="number">01</strong>
-                    :
+                    /
+                    <span className="font-16">::: Section</span>
                 </div>
-                <div className="flex">
-                    <div className="">
-                        
-                    </div>
-                    <div className="left-text">
-                        Project<br/>
-                        <strong>
-                            Portfolirate
-                        </strong>
-                    </div>
+                <div className="left-text padding-8">
+                    Project<br/>
+                    <strong>
+                    <span className="accent"> P</span>
+                        ortfolirate
+                    </strong>
                 </div>
             </div>
-            <div>
-                <div className="flex def-margin-top justify-bw font-16">
-                    <span>solve Inteligence, Think Different</span>
-                    <span>Rerur, LLC</span>
+            <div className="flex def-margin-top ">
+                <div>
+                    <span className="flex font-16">
+                        solve Inteligence, Think Different
+                    </span>
+                    <div className="flex def-margin-top font-16">
+                        <span className="right-text left-margin-5 font-32">
+                            from<br/> 
+                            <strong className="font-32">Rerurate_514</strong>
+                        </span>
+                    </div>
                 </div>
-                <div className="flex def-margin-top justify-bw font-16">
-                    <span>solve Inteligence, Think Different</span>
-                    <span>Rerur, LLC</span>
+                <div className="left-margin-20 left-text">
+                    Rerur, LLC<br/><br/><br/>
+                    Institute Of Tohoku Univ.
                 </div>
             </div>
         </div>
