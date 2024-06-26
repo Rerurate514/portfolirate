1a1ef9bdc7fbacf5ff7c5343e2561a9edccd0a11
Rerurate514 <rerurateyuto@gmail.com>
2024-06-27 01:02:41 +0900

    色の変更

--- src/components/hex.tsx
+++ src/components/hex.tsx
@@ -1,5 +1,5 @@
 export const Hex = (prop: {content: string}) => {
-    return <div className="hexagon-1_hexagon futura-font-bold back-black margin-32">
+    return <div className="hexagon-1_hexagon futura-font-bold back-green margin-32">
         <span className="back-white">{prop.content}</span>
     </div>
 
