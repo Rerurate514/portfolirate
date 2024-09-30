5f5eb647d450a5c0b2871a8b0a0e446d4c5606b6
Rerurate514 <rerurateyuto@gmail.com>
2024-09-30 21:05:18 +0900

    3dBoxの作成

---
+++ src/components/3dBox.tsx
@@ -0,0 +1,14 @@
+export const ThreeDbox = () => {
+    return <div>
+        <div id="scene">
+            <div className="boxBase rotate3d futura-font-light white font-16">
+                <div className="top">Flutter</div>
+                <div className="bottom">Kotlin</div>
+                <div className="front">React</div>
+                <div className="back">JS</div>
+                <div className="left">Rust</div>
+                <div className="right">Obsidian</div>
+            </div>
+        </div>
+    </div>
+}
