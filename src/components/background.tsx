ee0f5e1b249b05a42dff59d8094c0296b8d4c39a
Rerurate514 <rerurateyuto@gmail.com>
2025-06-09 03:09:42 +0900

    Orbitコンポーネントを追加

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Hexes } from './hexes';
 
 import "../css/background.scss";
+import "../css/orbit.scss";
 
 interface Point {
     x: number;
@@ -10,6 +11,9 @@ interface Point {
 
 export const BackGround = () => {
     return <div className='width-max relative'>
+        <div className='absolute'>
+            <Orbit />
+        </div>
         <div className='stars absolute'>
             <Star r={4} angle={195 + (Math.random() * 10) - (Math.random() * 10)} speedFactor={Math.random()}/>
             <Star r={5} angle={200 + (Math.random() * 10) - (Math.random() * 10)} speedFactor={Math.random()}/>
@@ -69,7 +73,7 @@ const Star = (props: {r: number, angle: number, speedFactor: number}) => {
 
         }, 16); // 60fpsのアニメーションを実現するため、16msごとに更新 256, 1300
         return () => clearInterval(interval);
-      }, [point]);
+    }, [point]);
 
     return <div className='star absolute'>
         <svg width={window.innerWidth * 0.95 } height={window.innerHeight}>
@@ -78,3 +82,27 @@ const Star = (props: {r: number, angle: number, speedFactor: number}) => {
         </svg>
     </div>
 }
+
+const Orbit = () => {
+    const SVG_COLOR = '#AEB833';
+    const SVG_COLOR_BL = '#20191946';
+
+    return (
+        <div className="relative">
+            <svg width={"2000px"} height={"2000px"} fill='transparent'>
+                <circle className="orbit-cir-1" stroke={SVG_COLOR_BL} strokeWidth={"6px"} cx={100} cy={1400} r={1700}/>
+                <circle className="orbit-cir-2" stroke={SVG_COLOR_BL} strokeWidth={"5px"} cx={100} cy={1400} r={1675}/>
+                <circle className="orbit-cir-3" stroke={SVG_COLOR_BL} strokeWidth={"4px"} cx={100} cy={1400} r={1650}/>
+                <circle className="orbit-cir-4" stroke={SVG_COLOR_BL} strokeWidth={"3px"} cx={100} cy={1400} r={1625}/>
+                <circle className="orbit-cir-5" stroke={SVG_COLOR_BL} strokeWidth={"2px"} cx={100} cy={1400} r={1600}/>
+                
+                <circle className="orbit-cir-6" stroke={SVG_COLOR} strokeWidth={"32px"} cx={200} cy={200} r={1530} />
+                <circle className="orbit-cir-7" stroke={SVG_COLOR} strokeWidth={"16px"} cx={200} cy={200} r={1400} />
+                <circle className="orbit-cir-8" stroke={SVG_COLOR} strokeWidth={"8px"} cx={200} cy={200} r={1300} />
+                <circle className="orbit-cir-9" stroke={SVG_COLOR} strokeWidth={"4px"} cx={200} cy={200} r={1200} />
+                <circle className="orbit-cir-10" stroke={SVG_COLOR} strokeWidth={"2px"} cx={200} cy={200} r={1100} />
+                <circle className="orbit-cir-11" stroke={SVG_COLOR} strokeWidth={"1px"} cx={200} cy={200} r={1000} />
+            </svg>
+        </div>
+    );
+};
